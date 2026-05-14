611eb70ddb2977b53612cd7a0de2d4a89fe95ad6
NULLs-Group <davirc65@gmail.com>
2026-05-14 17:04:15 -0300

    as

---
+++ src/Config/Painel.lua
@@ -0,0 +1,62 @@
+local Painel = {
+
+	PaineAdm = {
+		"PainelAdm";
+		"Open";
+		"Frame1";
+		"Frame2";
+		"Frame3";
+		"Frame4";
+		"Frame5";
+		"Frame6";
+		"Frame7";
+	};
+
+	----------------------------------- Player ---------------------------------------------
+
+	[3733387773] = { --- Id do player (drccastr57)
+
+		Liberados = {
+			"Frame1";
+        	"Frame2";
+			"Frame3";
+			"Frame4";
+			"Frame5";
+			"Frame6";
+			"Frame7";
+		};
+
+		Bloqueados = {
+			"PainelAdm";
+			"Open";
+
+		};
+	};
+
+	----------------------------------- Painel ---------------------------------------------
+
+    	[23412] = { --- Id do player ()
+
+		Liberados = {
+			
+		};
+
+		Bloqueados = {
+			"PainelAdm";
+			"Open";
+            "Frame1";
+			"Frame2";
+			"Frame3";
+			"Frame4";
+			"Frame5";
+			"Frame6";
+			"Frame7";
+		};
+	};
+
+	----------------------------------- Painel ---------------------------------------------
+
+}
+
+print("Configurações do painel carregadas com sucesso!")
+return Painel
